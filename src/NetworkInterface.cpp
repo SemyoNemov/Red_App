7bedea7bace9dce58e2e52bf63da664e300c8618
Semyon Nemov <semyonkan24@gmail.com>
2026-01-23 20:33:43 +0700

    Продолжение работы над функцией сканирования интерфейсов

--- src/NetworkInterface.cpp
+++ src/NetworkInterface.cpp
@@ -3,9 +3,10 @@
 #include <string>
 #include <fstream>
 #include <filesystem>
+#include <iostream>
 using namespace std;
 namespace fs = std::filesystem;
-std::vector<NetworkInterface> scanNetworkInterface()
+vector<NetworkInterface> scanNetworkInterface()
 {
     vector<NetworkInterface> interfaces;
     NetworkInterface interface;
@@ -25,5 +26,20 @@ std::vector<NetworkInterface> scanNetworkInterface()
         if (index_file) {index_file>>interface.index;}
         fstream mtu_file(dir.path() / "mtu");
         if (mtu_file) {mtu_file>>interface.mtu;}
+        fstream devtype(dir.path() / "uevent");
+        if (devtype) {
+            string x((std::istreambuf_iterator<char>(devtype)),
+            std::istreambuf_iterator<char>());
+            if (x.find("DEVTYPE=")!=string::npos){
+                interface.type=x.substr(x.find("DEVTYPE=")+8, x.find('\n', x.find("DEVTYPE=")+8)-8);;
+            }
+
+        }
     }
+}
+int main()
+{
+    vector<NetworkInterface> scan=scanNetworkInterface();
+    cout<<scan[0].type;
+return 0;
 }
